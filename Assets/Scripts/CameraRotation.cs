41681a61cc6296a00478bf9138ec361a2789be45
grapes0 <128805851+grapes0@users.noreply.github.com>
2024-03-20 21:20:49 +0300

    Player Tweaks
    Made the player and attached prefabs work good with probuilder geometry

--- Assets/Scripts/CameraRotation.cs
+++ Assets/Scripts/CameraRotation.cs
@@ -9,12 +9,6 @@ public class CameraRotation : MonoBehaviour
     public float minAngle;
     public float maxAngle;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
